8be74b765db0288534f8b2e2a98e4a047e323634
Araham7 <arahamabeddin7@gmail.com>
2024-11-23 23:13:12 +0530

    Update comment in each line of the code of 01_ESP8266_mDNS_Basic_Code

--- 01_ESP8266_mDNS_Basic_Code/01_ESP8266_mDNS_Basic_Code.ino
+++ 01_ESP8266_mDNS_Basic_Code/01_ESP8266_mDNS_Basic_Code.ino
@@ -1,39 +1,39 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
+#include <ESP8266WiFi.h>    // Include the ESP8266 Wi-Fi library
+#include <ESP8266mDNS.h>    // Include the mDNS library for ESP8266
 
-const char* ssid = "Laptop";
-const char* password = "8918172922";
-const char* hostname = "esp8266";
+const char* ssid = "Laptop";           // Wi-Fi SSID
+const char* password = "8918172922";   // Wi-Fi password
+const char* hostname = "esp8266";      // mDNS hostname
 
 void setup() {
-  // Start serial communication
-  Serial.begin(9600);
-  Serial.println("\nStarting ESP8266 mDNS Basic Code...");
+  // Start serial communication with 9600 baud rate
+  Serial.begin(9600); 
+  Serial.println("\nStarting ESP8266 mDNS Basic Code...");  // Print start message
+  delay(35);  // Short delay for stability
 
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi..");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  // Connect to Wi-Fi network
+  WiFi.begin(ssid, password);  
+  Serial.print("Connecting to WiFi..");   // Print connecting message
+  while (WiFi.status() != WL_CONNECTED) {  // Wait until connected to Wi-Fi
+    delay(500);  // Wait for 500ms
+    Serial.print(".");  // Print dot to indicate connection progress
   }
-  Serial.println("\nWiFi connected.");
-  Serial.printf("IP Address: %s\n", WiFi.localIP().toString().c_str());
+  Serial.println("\nWiFi connected.");  // Print Wi-Fi connected message
+  Serial.printf("IP Address: %s\n", WiFi.localIP().toString().c_str());  // Print the ESP8266 IP address
 
-  // Start mDNS
-  if (MDNS.begin(hostname)) {
-    Serial.println("mDNS responder started.");
-    Serial.printf("You can access your ESP8266 at http://%s.local\n", hostname);
+  // Start mDNS with the defined hostname
+  if (MDNS.begin(hostname)) {  
+    Serial.println("mDNS responder started.");  // If mDNS started successfully
+    Serial.printf("You can access your ESP8266 at http://%s.local\n", hostname);  // Print mDNS URL
   } else {
-    Serial.println("Error starting mDNS responder.");
+    Serial.println("Error starting mDNS responder.");  // Print error message if mDNS fails
   }
 }
 
 void loop() {
-  // Handle mDNS queries
-  MDNS.update();
+  // Continuously handle mDNS queries
+  MDNS.update();  
 }
-
 
 
 
